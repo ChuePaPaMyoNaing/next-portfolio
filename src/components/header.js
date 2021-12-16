47c6a018d25c11b7a2c84822b92c08ca869792a2
chuepapamyonaing <chuepapamyonaing@cybermissions.co.jp>
2021-12-16 14:03:42 +0900

    pagination

--- src/components/header.js
+++ src/components/header.js
@@ -3,23 +3,21 @@ import Image from 'next/image'
 import * as style from "../styles/common.module.scss"
 
 const Header = () => {
-  return(
-    <header className={style.headerWrapper}>
-      <div className={style.container}>
-        <div className={style.flexContainer}>
-          <Link href="/">
-            <a>
-              <Image src="/images/logo.png" alt="logo"
-                width={50} height={50}
-              />
-            </a>
-          </Link>
-          <ul>
-            <li><Link href="/blog"><a>Blog</a></Link></li>
-            <li><Link href="/contact"><a>Contact</a></Link></li>
-          </ul>
+  return (
+     <header className={style.headerWrapper}>
+       <div className={style.container}>
+          <div className={style.flexContainer}>
+            <Link href="/">
+              <a> 
+                <Image src="/images/logo.png" alt="logo" width={50} height={50} />
+              </a>
+            </Link>
+            <ul>
+              <li><Link href="/blog"><a>Blog</a></Link></li>
+              <li><Link href="/contact"><a>Contact</a></Link></li>
+            </ul>
+          </div>
         </div>
-      </div>
     </header>
   )
 }
