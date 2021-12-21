e8d269066ab5c4973ebc5b8e08a0dee9af9a42bf
chuepapamyonaing <chuepapamyonaing@cybermissions.co.jp>
2021-12-21 15:39:15 +0900

    add js

---
+++ src/pages/example.js
@@ -0,0 +1,16 @@
+const Example = () => {
+  return (
+    <div>
+      <h1 className="test">はじめてつくるNext.jsサイト</h1>
+      <style jsx>{`
+        .test {
+          color: red;
+          magin-top: 50px;
+        }
+       `}
+      </style>
+    </div>
+  )
+}
+
+export default Example
